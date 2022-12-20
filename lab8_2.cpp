da9aa2fe3a67449d6cb73c545da70a01536fb145
KonLew <surangrat231@gmail.com>
2022-12-20 16:12:54 +0700

    lab8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,18 +1,33 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main(){
+    
+    string name,mov,day,a;
+    int gear;
+    string y = "Fahsai: ";
+    string z = "\n";
+    
+    cout << y << "Sawadee ka...Can you tell me your name?" << z;
+    cout << "?????: ";
+    getline(cin,name);
+    cout << y << "Wow!!! " << name << " is a really cool name." << z;
+    cout << y << "I think you are an Engineering student. What is your student ID?" << z;
+    cout << name << ": ";
+    cin >> gear;
+    cin.ignore();
+    cout << y << "I think you may be GEAR "<< gear/10000000-12 << ". I have a free movie ticket for you." << z;
+    cout << y << "Let's go to the cinema together!!!" << z;
+    cout << y << "What movie do you want to watch?" << z;
+    cout << name << ": ";
+    getline(cin,mov);
+    cout << y << "So....which day are you free to go with me?" << z;
+    cout << name << ": ";
+    getline(cin,day);
+    cout << y << day <<"....that is OK!!! I'm looking forward to watching " << mov <<" with you." << z;
+    cout << name << ": ";
+    cin >> a;
+    cout << y << "555+ see you "<< day << ". Bye Bye "<<'\\'<<"(^ ^)/";
+return 0;
+}
